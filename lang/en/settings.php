9249e31a7b17080723b3523e2621a78e6e789101
Christoph Ziehr <christoph@christoph-ziehr.de>
2018-12-09 15:24:46 +0100

    Add files via upload

---
+++ lang/en/settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * english language file for nsiconinsearch plugin
+ *
+ * @author Christoph Ziehr <info@einsatzleiterwiki.de>
+ */
+
+// keys need to match the config setting name
+// $lang['fixme'] = 'FIXME';
+
