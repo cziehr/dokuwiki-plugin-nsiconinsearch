35274a78535f6083ec2ad6ac3e553533c49115a4
Christoph Ziehr <christoph@christoph-ziehr.de>
2018-12-09 15:22:17 +0100

    Add files via upload

---
+++ action.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * DokuWiki Plugin nsiconinsearch (Action Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  Christoph Ziehr <info@einsatzleiterwiki.de>
+ */
+
+// must be run within Dokuwiki
+if (!defined('DOKU_INC')) {
+    die();
+}
+
+class action_plugin_nsiconinsearch extends DokuWiki_Action_Plugin
+{
+
+    /**
+     * Registers a callback function for a given event
+     *
+     * @param Doku_Event_Handler $controller DokuWiki's event controller object
+     *
+     * @return void
+     */
+    public function register(Doku_Event_Handler $controller)
+    {
+        $controller->register_hook('SEARCH_RESULT_FULLPAGE', 'BEFORE', $this, 'handle_search_result_fullpage');
+   
+    }
+
+    /**
+     * [Custom event handler which performs action]
+     *
+     * Called for event:
+     *
+     * @param Doku_Event $event  event object by reference
+     * @param mixed      $param  [the parameters passed as fifth argument to register_hook() when this
+     *                           handler was registered]
+     *
+     * @return void
+     */
+    public function handle_search_result_fullpage(Doku_Event $event, $param)
+    {
+
+
+        $event->$data['resultHeader'][] = '<p>TEST</p>';
+
+    }
+
+}
+
