e3cdcdfca4599ead6a2b09faefcd00fe1a43459f
Christoph Ziehr <christoph@christoph-ziehr.de>
2018-12-09 15:23:50 +0100

    Create lang.php

---
+++ lang/en/lang.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * English language file for nsiconinsearch plugin
+ *
+ * @author Christoph Ziehr <info@einsatzleiterwiki.de>
+ */
+
+// menu entry for admin plugins
+// $lang['menu'] = 'Your menu entry';
+
+// custom language strings for the plugin
+// $lang['fixme'] = 'FIXME';
