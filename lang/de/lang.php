5b02bc7ea4c74193e81390dd13245dc74927781f
Christoph Ziehr <christoph@christoph-ziehr.de>
2018-12-17 00:53:47 +0100

    Add files via upload

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,1 +1,13 @@
-Test
+<?php
+/**
+ * English language file for nsiconinsearch plugin
+ *
+ * @author Christoph Ziehr <info@einsatzleiterwiki.de>
+ */
+
+// menu entry for admin plugins
+// $lang['menu'] = 'Your menu entry';
+
+// custom language strings for the plugin
+// $lang['fixme'] = 'FIXME';
+
---
+++ lang/de/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * German language file for nsiconinsearch plugin
+ *
+ * @author Christoph Ziehr <info@einsatzleiterwiki.de>
+ */
+
+$lang['in_namespaces'] = 'Namensräume, bei denen das Icon angezeigt werden soll (mehrere Namensräume durch Leerzeichen trennen).';
+
